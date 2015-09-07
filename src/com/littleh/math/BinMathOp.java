596988472768457c007ffbb4e80b8a62d706a324
DennyBoy <denny.headrick@gmail.com>
2015-09-07 14:12:32 -0500

    Update

--- src/com/littleh/math/BinMathOp.java
+++ src/com/littleh/math/BinMathOp.java
@@ -3,7 +3,7 @@ package com.littleh.math;
 /**
  * Created by Dennis on 2015-09-07.
  */
-public interface MathOp {
+public interface BinMathOp {
     public double run(double a, double b);
 
 }
--- src/com/littleh/math/Operations.java
+++ src/com/littleh/math/Operations.java
@@ -1,23 +1,37 @@
 package com.littleh.math;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 /**
- * Created by Dennis on 2015-09-07.
+ * Operations adds a series of mathematical operations <br>
+ * to the mathOps HashMap.  The class utilizes Lambda <br>
+ * expressions and requires Java 8 to run.
+ *
+ * @author Denny
+ * @since 9/7/2015
  */
 public class Operations {
 
-    HashMap<String, MathOp> mathOps;
+    HashMap<String, Object> mathOps;
 
 
 
     public Operations(){
-      mathOps = new HashMap<String, MathOp>();
+      mathOps = new HashMap<String,Object>();
+      mathOps.put("add", (BinMathOp) (a, b)->a+b);
+      mathOps.put("subtract", (BinMathOp)(a,b)->a-b);
+      mathOps.put("multiply", (BinMathOp) (a,b)->a*b);
+      mathOps.put("divide", (BinMathOp) (a,b)->{
+                                    double r=0;
+                                    if(b!=0){
+                                     r=a/b;}
+                                    return r;});
 
 
     }
 
+    public static void main(String[] args){
+
+    }
 
 }
